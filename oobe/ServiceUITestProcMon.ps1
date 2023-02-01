8278a7b22d0edf9caf2bd4fe2a1f93314910ff4d
Joël Ammann <joel.ammann@leuchterag.ch>
2023-02-01 11:15:29 +0100

    update

--- oobe/ServiceUITestProcMon.ps1
+++ oobe/ServiceUITestProcMon.ps1
@@ -96,58 +96,6 @@ $scheduledTaskRunAsSystem = @"
 </Task>
 "@
 
-$scheduledTaskRunAsDefaultuser0 = @"
-<?xml version="1.0" encoding="UTF-16"?>
-<Task version="1.6" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
-  <RegistrationInfo>
-    <Date>2014-10-01T00:00:00.0000000</Date>
-    <Author>System</Author>
-    <Description>everydayintech</Description>
-    <URI>\OSD ServiceUI</URI>
-  </RegistrationInfo>
-  <Triggers>
-    <BootTrigger>
-      <Enabled>true</Enabled>
-    </BootTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id="LocalSystem">
-      <UserId>S-1-5-21-561738137-981327493-1153898575-1000</UserId>
-      <LogonType>S4U</LogonType>
-      <RunLevel>HighestAvailable</RunLevel>
-    </Principal>
-  </Principals>
-  <Settings>
-    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
-    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
-    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
-    <AllowHardTerminate>true</AllowHardTerminate>
-    <StartWhenAvailable>false</StartWhenAvailable>
-    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
-    <IdleSettings>
-      <StopOnIdleEnd>true</StopOnIdleEnd>
-      <RestartOnIdle>false</RestartOnIdle>
-    </IdleSettings>
-    <AllowStartOnDemand>true</AllowStartOnDemand>
-    <Enabled>true</Enabled>
-    <Hidden>false</Hidden>
-    <RunOnlyIfIdle>false</RunOnlyIfIdle>
-    <DisallowStartOnRemoteAppSession>false</DisallowStartOnRemoteAppSession>
-    <UseUnifiedSchedulingEngine>true</UseUnifiedSchedulingEngine>
-    <WakeToRun>false</WakeToRun>
-    <ExecutionTimeLimit>PT72H</ExecutionTimeLimit>
-    <Priority>7</Priority>
-  </Settings>
-  <Actions Context="LocalSystem">
-    <Exec>
-      <Command>C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe</Command>
-      <Arguments>-ExecutionPolicy ByPass C:\OSDCloud\temp\ServiceUIScheduledTask.ps1</Arguments>
-      <WorkingDirectory>C:\OSDCloud\temp</WorkingDirectory>
-    </Exec>
-  </Actions>
-</Task>
-"@
-
 $scheduledTaskXml = $scheduledTaskRunAsSystem
 
 Register-ScheduledTask -TaskName "OSD ServiceUI" -Xml $scheduledTaskXml -Force
