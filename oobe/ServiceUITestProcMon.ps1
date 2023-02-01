fd3c409e1d3ad9a9e8f371f91656e536f4108613
Joël Ammann <joel.ammann@leuchterag.ch>
2023-02-01 11:02:59 +0100

    update

--- oobe/ServiceUITestProcMon.ps1
+++ oobe/ServiceUITestProcMon.ps1
@@ -163,6 +163,38 @@ $ScheduledTaskScript = {
     # Write-Host -ForegroundColor DarkGray "Unregistering OSD ServiceUI Scheduled Task"
     # Unregister-ScheduledTask -TaskName "OSD ServiceUI" -Confirm:$false
 
+    Function Set-WindowStyle 
+    {
+        param
+        (
+            [Parameter()]
+            [ValidateSet('FORCEMINIMIZE', 'HIDE', 'MAXIMIZE', 'MINIMIZE', 'RESTORE', 
+                'SHOW', 'SHOWDEFAULT', 'SHOWMAXIMIZED', 'SHOWMINIMIZED', 
+                'SHOWMINNOACTIVE', 'SHOWNA', 'SHOWNOACTIVATE', 'SHOWNORMAL')]
+            $Style = 'SHOW',
+            [Parameter()]
+            $MainWindowHandle = (Get-Process -Id $pid).MainWindowHandle
+        )
+
+        $WindowStates = @{
+            FORCEMINIMIZE = 11; HIDE = 0
+            MAXIMIZE = 3; MINIMIZE = 6
+            RESTORE = 9; SHOW = 5
+            SHOWDEFAULT = 10; SHOWMAXIMIZED = 3
+            SHOWMINIMIZED = 2; SHOWMINNOACTIVE = 7
+            SHOWNA = 8; SHOWNOACTIVATE = 4
+            SHOWNORMAL = 1
+        }
+        Write-Verbose ("Set Window Style {1} on handle {0}" -f $MainWindowHandle, $($WindowStates[$style]))
+
+        $Win32ShowWindowAsync = Add-Type -memberDefinition @" 
+[DllImport("user32.dll")] 
+public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+"@ -name "Win32ShowWindowAsync" -namespace Win32Functions -PassThru
+
+        $Win32ShowWindowAsync::ShowWindowAsync($MainWindowHandle, $WindowStates[$Style]) | Out-Null
+    }
+
     $ProcessAttachTo = 'WWAHost.exe'
 
     do {
@@ -178,6 +210,8 @@ $ScheduledTaskScript = {
     Write-Host -ForegroundColor DarkGray "Found $ProcessAttachTo running, launching ServiceUI64.exe to start Procmon64.exe"
     Write-Host -ForegroundColor DarkGray "ServiceUI64.exe -process:$ProcessAttachTo C:\OSDCloud\temp\Procmon64.exe"
     C:\OSDCloud\temp\ServiceUI64.exe -process:RuntimeBroker.exe C:\OSDCloud\temp\Procmon64.exe
+
+    Get-Process -Name WWAHost       | %{Set-WindowStyle MINIMIZE $PSItem.MainWindowHandle}
 
     Stop-Transcript
 }
