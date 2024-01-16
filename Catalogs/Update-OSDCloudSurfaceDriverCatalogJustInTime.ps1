d993d3bd297eb6c202d37e3850936e0419eb4006
Joël Ammann <joel.ammann@leuchterag.ch>
2024-01-16 10:52:10 +0100

    update

--- Catalogs/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1
+++ Catalogs/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1
@@ -23,23 +23,25 @@ function Update-OSDCloudSurfaceDriverCatalogJustInTime {
             Write-Warning "DriverPack for $($DriverPack.Name) ($($DriverPack.Product)) is not available at $($DriverPack.Url)"
             Write-Verbose $Err.Exception.Message
 
-            Write-Warning "Download Center URL: $($DriverPack.DownloadCenter)"
+            # Write-Warning "Download Center URL: $($DriverPack.DownloadCenter)"
             try {
                 $DownloadCenter = Invoke-WebRequest -Uri $DriverPack.DownloadCenter -UseBasicParsing | Select-Object -ExpandProperty Content
                 [datetime]$UpdatedOn = $DownloadCenter -match '"detailsSection_file_date":"([\/\d]+)"' | ForEach-Object { $Matches[1] }
                 if (-NOT $UpdatedOn) { throw }
-                Write-Warning "Download updated on: $($UpdatedOn.ToString('d'))"
+                # Write-Warning "Download updated on: $($UpdatedOn.ToString('d'))"
 
                 $DriverOsVersion = $DriverPack.Url -match '_(Win[\d]{2})_' | ForEach-Object { $Matches[1] }
                 $MsiDownloadsMatch = [regex]::Matches($DownloadCenter, "`"url`":`"(https:[\w\-\/\.]+\.msi)`"")
                 $UpdatedDriverUrl = ($MsiDownloadsMatch | Where-Object { $_.Value -match $DriverOsVersion }).Groups[1].Value
-                
-                Write-Host "New Download URL: $($UpdatedDriverUrl)"
 
                 #Replace dead download links in CloudDriverPacks.json
                 if ($UpdateDriverPacksJson) {
                     $content = [System.IO.File]::ReadAllText($LocalCloudDriverPacksJson).Replace($DriverPack.Url, $UpdatedDriverUrl)
                     [System.IO.File]::WriteAllText($LocalCloudDriverPacksJson, $content)
+                    Write-Host "Updated Download URL to: $($UpdatedDriverUrl)"
+                }
+                else {
+                    Write-Host "New Download URL: $($UpdatedDriverUrl)"
                 }
             }
             catch {
