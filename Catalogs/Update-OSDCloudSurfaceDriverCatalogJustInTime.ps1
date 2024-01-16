d750ff09cc5ba483e8e72807b4070e314fe1a79c
Joël Ammann <joel.ammann@leuchterag.ch>
2024-01-16 21:29:16 +0100

    update logic

--- Catalogs/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1
+++ Catalogs/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1
@@ -15,38 +15,47 @@ function Update-OSDCloudSurfaceDriverCatalogJustInTime {
     foreach ($DriverPack in $Catalog) {
         try {
             $Response = Invoke-WebRequest -Method Head -Uri $DriverPack.Url -UseBasicParsing -ErrorAction Stop
-
             Write-Verbose ('{0} {1}' -f $Response.StatusCode, $DriverPack.Url)
         }
-        catch {
+        catch [System.Net.WebException]{
             $Err = $_
             Write-Warning "DriverPack for $($DriverPack.Name) ($($DriverPack.Product)) is not available at $($DriverPack.Url)"
             Write-Verbose $Err.Exception.Message
 
-            # Write-Warning "Download Center URL: $($DriverPack.DownloadCenter)"
+            Write-Verbose "Download Center URL: $($DriverPack.DownloadCenter)"
             try {
                 $DownloadCenter = Invoke-WebRequest -Uri $DriverPack.DownloadCenter -UseBasicParsing | Select-Object -ExpandProperty Content
-                [datetime]$UpdatedOn = $DownloadCenter -match '"detailsSection_file_date":"([\/\d]+)"' | ForEach-Object { $Matches[1] }
-                if (-NOT $UpdatedOn) { throw }
-                # Write-Warning "Download updated on: $($UpdatedOn.ToString('d'))"
 
                 $DriverOsVersion = $DriverPack.Url -match '_(Win[\d]{2})_' | ForEach-Object { $Matches[1] }
-                $MsiDownloadsMatch = [regex]::Matches($DownloadCenter, "`"url`":`"(https:[\w\-\/\.]+\.msi)`"")
+                $MsiDownloadsMatch = [regex]::Matches($DownloadCenter, '"url":"(https:[\w\-\/\.]+\.msi)"')
                 $UpdatedDriverUrl = ($MsiDownloadsMatch | Where-Object { $_.Value -match $DriverOsVersion }).Groups[1].Value
+                $UpdateDriverFilename = $UpdatedDriverUrl.Split('/') | Select-Object -Last 1
 
                 #Replace dead download links in CloudDriverPacks.json
                 if ($UpdateDriverPacksJson) {
-                    $content = [System.IO.File]::ReadAllText($LocalCloudDriverPacksJson).Replace($DriverPack.Url, $UpdatedDriverUrl)
+                    $content = [System.IO.File]::ReadAllText($LocalCloudDriverPacksJson)
+
+                    Write-Verbose "Replacing [$($DriverPack.Url)] with [$($UpdatedDriverUrl)]"
+                    $content = $content.Replace($DriverPack.Url, $UpdatedDriverUrl)
+
+                    Write-Verbose "Replacing [$($DriverPack.FileName)] with [$($UpdateDriverFilename)]"
+                    $content = $content.Replace($DriverPack.FileName, $UpdateDriverFilename)
+
                     [System.IO.File]::WriteAllText($LocalCloudDriverPacksJson, $content)
-                    Write-Host "Updated Download URL to: $($UpdatedDriverUrl)"
+                    
+                    Write-Host "Updated download URL to: $($UpdatedDriverUrl)"
                 }
                 else {
-                    Write-Host "New Download URL: $($UpdatedDriverUrl)"
+                    Write-Host "New download URL: $($UpdatedDriverUrl)"
                 }
             }
             catch {
                 Write-Warning 'Unable to retrieve updated download URL'
             }
+        }
+        catch {
+            $Err = $_
+            Write-Warning "$($Err.Exception.Message) [$($Err.Exception.GetType().FullName)]"
         }
     }
 }
